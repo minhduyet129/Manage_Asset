265efdbc1fd1944e8022231e29f0e4cc9dddad88
chinhtkt <chinhtkt>
2021-05-11 11:26:29 +0700

    co tinh lam conflint ne hihi

--- RookieOnlineAssetManagement/Models/UserModel.cs
+++ RookieOnlineAssetManagement/Models/UserModel.cs
@@ -2,7 +2,7 @@
 {
     public class UserModel
     {
-        //commit
+        //commit moi hehe
         public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
