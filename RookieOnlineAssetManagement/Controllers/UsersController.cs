2cc0222b1a6bc2d64fd6a8308ffedc4e6978022e
Dat Le <letrongdat487@gmail.com>
2021-05-13 19:48:47 +0700

    Validation for Create user API

--- RookieOnlineAssetManagement/Controllers/UsersController.cs
+++ RookieOnlineAssetManagement/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using RookieOnlineAssetManagement.Data;
 using RookieOnlineAssetManagement.Entities;
 using RookieOnlineAssetManagement.Models;
+using RookieOnlineAssetManagement.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,6 +57,46 @@ namespace RookieOnlineAssetManagement.Controllers
             return password;
         }
 
+        public UserResponse UserValidation(UserModel model)
+        {
+            var error = new List<object> { };
+            int statusCode = 0;
+            string message = null;
+
+            if (DateTime.Now.Year - model.DoB.Year < 18)
+            {
+                statusCode = 422;
+                error.Add(new { doB = "User is under 18. Please select a different date" });
+            }
+
+            if (model.DoB.Year - model.JoinedDate.Year > 0)
+            {
+                statusCode = 422;
+                error.Add(new { joinedDate = "Joined date is not later than Date of Birth. Please select a different date" });
+            }
+
+            if (model.JoinedDate.DayOfWeek == DayOfWeek.Saturday || model.JoinedDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                statusCode = 422;
+                error.Add(new { joinedDate = "Joined date is Saturday or Sunday. Please select a different date" });
+            }
+
+            if (error.Count() == 0)
+            {
+                statusCode = 201;
+                message = "Created user successfully";
+            }
+            else message = "Created fail";
+
+            var response = new UserResponse
+            {
+                StatusCode = statusCode,
+                Error = error,
+                Message = message
+            };
+            return response;
+        }
+
         [HttpGet("{id}")]
         public IActionResult GetUser(int id)
         {
@@ -74,6 +115,13 @@ namespace RookieOnlineAssetManagement.Controllers
         {
             string userName = AutoGenerateUserName(model.FirstName, model.LastName);
             string password = AutoGeneratePassword(userName, model.DoB);
+
+            var userValidation = UserValidation(model);
+
+            if (userValidation.StatusCode == 422)
+            {
+                return StatusCode(422, userValidation);
+            }    
 
             var user = new ApplicationUser
             {
@@ -97,13 +145,13 @@ namespace RookieOnlineAssetManagement.Controllers
             currentUser.StaffCode = staffCode;
             _dbContext.Users.Update(currentUser);
             _dbContext.SaveChanges();
-            return Ok(password);
+            return StatusCode(userValidation.StatusCode, userValidation);
         }
 
-        //[HttpPut]
-        //public IActionResult UpdateStaffCode(int id)
-        //{
-
-        //}
+        [HttpPost("test")]
+        public IActionResult Test(UserModel model)
+        {
+            return Ok(UserValidation(model));
+        }
     }
 }
