23aec701ff8b10d97d7c16a4099238e83ebb66c0
minhduyet129 <minhduyet129@gmail.com>
2021-05-14 11:30:21 +0700

    remove conflict update usermodel

--- RookieOnlineAssetManagement/Models/UserModel.cs
+++ RookieOnlineAssetManagement/Models/UserModel.cs
@@ -5,16 +5,13 @@ namespace RookieOnlineAssetManagement.Models
 {
     public class UserModel
     {
-        //commit moi hehe
-        public string Id { get; set; }
-        public string StaffCode { get; set; }
-        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DoB { get; set; }
         public DateTime JoinedDate { get; set; }
         public Gender Gender { get; set; }
-        
         public string Location { get; set; }
+        public int Id { get; set; }
+        public string StaffCode { get; set; }
     }
 }
