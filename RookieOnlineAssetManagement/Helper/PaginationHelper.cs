c47423845de180d0688e1410c9580692e80a1b0d
Dat Le <letrongdat487@gmail.com>
2021-05-14 15:39:46 +0700

    Get User with Pagination

---
+++ RookieOnlineAssetManagement/Helper/PaginationHelper.cs
@@ -0,0 +1,26 @@
+﻿using RookieOnlineAssetManagement.Filter;
+using RookieOnlineAssetManagement.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Helper
+{
+    public class PaginationHelper
+    {
+        public static PagedResponse<List<T>> CreatePagedReponse<T>(List<T> pagedData, PaginationFilter validFilter, int totalRecords, int statusCode)
+        {
+            var respose = new PagedResponse<List<T>>(pagedData, validFilter.PageNumber, validFilter.PageSize);
+
+            var totalPages = ((double)totalRecords / (double)validFilter.PageSize);
+            int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
+
+            respose.TotalPages = roundedTotalPages;
+            respose.TotalRecords = totalRecords;
+            respose.StatusCode = statusCode;
+
+            return respose;
+        }
+    }
+}
