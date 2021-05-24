455c39f6184a3a190d20a3279cdd20b0107cd452
Dat Le <letrongdat487@gmail.com>
2021-05-24 14:47:02 +0700

    View Assignment List

--- RookieOnlineAssetManagement/ClientApp/src/admin/pages/assignment/AssignmentHooks.js
+++
@@ -1,5 +0,0 @@
-export const useAssignment = () => {};
-export const useAssignments = () => {};
-export const useCreateAssignment = () => {};
-export const useDeleteAssignment = () => {};
-export const useUpdateAssignment = () => {};
