afa73e01d473d448ea580f891e344bc57e6f822b
Dat Le <letrongdat487@gmail.com>
2021-05-26 14:24:37 +0700

    Revert "Update View Assignment Detail"
    This reverts commit b57c886a0dd7de627b044a2d537fb2a17e97f491.

--- RookieOnlineAssetManagement/ClientApp/src/admin/pages/assignment/AssignmentTable.js
+++ RookieOnlineAssetManagement/ClientApp/src/admin/pages/assignment/AssignmentTable.js
@@ -2,15 +2,12 @@ import React from "react";
 import { useTable, usePagination } from "react-table";
 import { Link } from "react-router-dom";
 
-import "./Assignment.css";
 
 const UsersTable = ({
   columns,
   data,
   loading,
-  onSearch = () => {},
-  onFilterState = () => {},
-  onClickAssignment = () => {},
+  onSearch = () => {}
 }) => {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data }, usePagination);
@@ -20,38 +17,11 @@ const UsersTable = ({
       <div className="table__view">
         <h2>Manage Assignment</h2>
         <div className="table__view--search">
-          <div className="search">
-            <div className="filter-state">
-              <input
-                type="text"
-                placeholder="State"
-                onChange={(e) => onFilterState(e.target.value)}
-              />
-              <i className="bx bx-filter-alt" />
-            </div>
-            {/* <div className="state-options">
-              <label htmlFor="accepted" className="state-input">
-                <input id="accepted" type="radio" name="filterState" />
-                <div id="accepted">Accepted</div>
-              </label>
-              <label htmlFor="accepted" className="state-input">
-                <input id="accepted" type="radio" name="filterState" />
-                <div id="accepted">Accepted</div>
-              </label>
-              <label htmlFor="accepted" className="state-input">
-                <input id="accepted" type="radio" name="filterState" />
-                <div id="accepted">Accepted</div>
-              </label>
-              <label htmlFor="accepted" className="state-input">
-                <input id="accepted" type="radio" name="filterState" />
-                <div id="accepted">Accepted</div>
-              </label>
-              <label htmlFor="accepted" className="state-input">
-                <input id="accepted" type="radio" name="filterState" />
-                <div id="accepted">Accepted</div>
-              </label>
-            </div> */}
-          </div>
+          <form className="search">
+            <label />
+            <input type="text" placeholder="State" />
+            <i className="bx bx-filter-alt" />
+          </form>
           <div className="search">
             <label />
             <input
@@ -85,24 +55,13 @@ const UsersTable = ({
               </div>
             ) : (
               <tbody {...getTableBodyProps()}>
-                {rows.map((row, index) => {
+                {rows.map((row) => {
                   prepareRow(row);
                   return (
-                    <tr
-                      id="tr-hover"
-                      {...row.getRowProps()}
-                      onClick={(e) => {
-                        if (!e.target.closest("#Actions")) {
-                          onClickAssignment(row.original);
-                        }
-                      }}
-                    >
+                    <tr {...row.getRowProps()}>
                       {row.cells.map((cell) => {
                         return (
-                          <td
-                            id={cell.column.Header}
-                            {...cell.getCellProps()}
-                          >
+                          <td {...cell.getCellProps()}>
                             {cell.render("Cell")}
                           </td>
                         );
